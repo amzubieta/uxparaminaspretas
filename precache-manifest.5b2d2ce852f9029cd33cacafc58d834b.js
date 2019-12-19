d119d7a4075833b5904e258719507df00aac76a6
Angelica Zubieta <angelicazubieta@gmail.com>
2019-12-19 18:52:20 -0300

    Updates

---
+++ precache-manifest.5b2d2ce852f9029cd33cacafc58d834b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d995431f430a73682724001a6962bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60dfb31fb0419ab52cac",
+    "url": "/static/css/2.446880fa.chunk.css"
+  },
+  {
+    "revision": "0fcd091abf831c627b9b",
+    "url": "/static/css/main.8a4181d3.chunk.css"
+  },
+  {
+    "revision": "60dfb31fb0419ab52cac",
+    "url": "/static/js/2.bf8b830e.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.bf8b830e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fcd091abf831c627b9b",
+    "url": "/static/js/main.4623a53f.chunk.js"
+  },
+  {
+    "revision": "99c5357055a1f2363a03",
+    "url": "/static/js/runtime-main.0c8d979a.js"
+  },
+  {
+    "revision": "cb406396748fe3968783ab0ff70dbe74",
+    "url": "/static/media/2.cb406396.png"
+  },
+  {
+    "revision": "4623c9d79bbd978c407101017dce74c0",
+    "url": "/static/media/carrusel1.4623c9d7.jpg"
+  },
+  {
+    "revision": "3fbf7587ab8ade099fd67bf6d3340ab9",
+    "url": "/static/media/carrusel2.3fbf7587.jpg"
+  },
+  {
+    "revision": "49f97d05d684c7a655d165044da708b0",
+    "url": "/static/media/carrusel3.49f97d05.jpg"
+  },
+  {
+    "revision": "d5c5ca7bb15298f81ad5877f999700bd",
+    "url": "/static/media/karenCEO.d5c5ca7b.jpg"
+  },
+  {
+    "revision": "e1071d6b49bde7acbc2ff533423141c6",
+    "url": "/static/media/logo.e1071d6b.png"
+  },
+  {
+    "revision": "e04fe690cc0ddf97fdff08741d6b8691",
+    "url": "/static/media/tablero.e04fe690.jpeg"
+  }
+]);
